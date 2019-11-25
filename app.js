a4ab68d1a21df3713289059efb3751bb5ec6870c
tonyfang <q710370@gmail.com>
2019-11-25 20:36:11 +0800

    Set handlebars engine and index page, connect successful!

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const exphbs = require('express-handlebars')
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+
+app.listen(port, () => {
+  console.log(`the web is running on http://localhost:${port}`)
+})
