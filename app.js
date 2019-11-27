e4bef706b111b45ce0f70c07fa71de937d6cf6ed
tonyfang <q710370@gmail.com>
2019-11-27 19:16:47 +0800

    Add router

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const mongoose = require('mongoose')
 const port = 3000
 const exphbs = require('express-handlebars')
 const Record = require('./models/record')
+// 引用 body-parser
+const bodyParser = require('body-parser')
+// 設定 bodyParser
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
@@ -24,8 +28,37 @@ app.get('/', (req, res) => {
   })
 })
 
-app.get('/new', (req, res) => {
+app.get('/records/new', (req, res) => {
   return res.render('new')
+})
+
+app.post('/records', (req, res) => {
+  let icon = ''
+  if (req.body.category === '家居物業') {
+    icon += 'fa-home'
+  }
+  if (req.body.category === '交通出行') {
+    icon += 'fa-shuttle-van'
+  }
+  if (req.body.category === '休閒娛樂') {
+    icon += 'fa-grin-beam'
+  }
+  if (req.body.category === '餐飲食品') {
+    icon += 'fa-utensils'
+  }
+  if (req.body.category === '其他') {
+    icon += 'fa-pen'
+  }
+  const record = Record({
+    name: req.body.name,
+    category: icon,
+    date: req.body.date,
+    amount: req.body.amount
+  })
+  record.save(err => {
+    if (err) return console.error(err)
+    return res.redirect('/')
+  })
 })
 
 app.listen(port, () => {
