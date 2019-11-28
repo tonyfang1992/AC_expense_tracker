762dd4f80d6e6736c6f2a48032cbf77764882b29
tonyfang <q710370@gmail.com>
2019-11-28 08:52:16 +0800

    Add each category's pagination

--- app.js
+++ app.js
@@ -31,6 +31,31 @@ app.get('/', (req, res) => {
 app.get('/records/new', (req, res) => {
   return res.render('new')
 })
+app.get('/records/fa-home', (req, res) => {
+  Record.find({ category: 'fa-home' }, (err, records) => {
+    return res.render('fa-home', { records: records })
+  })
+})
+app.get('/records/fa-shuttle-van', (req, res) => {
+  Record.find({ category: 'fa-shuttle-van' }, (err, records) => {
+    return res.render('fa-shuttle-van', { records: records })
+  })
+})
+app.get('/records/fa-grin-beam', (req, res) => {
+  Record.find({ category: 'fa-grin-beam' }, (err, records) => {
+    return res.render('fa-grin-beam', { records: records })
+  })
+})
+app.get('/records/fa-utensils', (req, res) => {
+  Record.find({ category: 'fa-utensils' }, (err, records) => {
+    return res.render('fa-utensils', { records: records })
+  })
+})
+app.get('/records/fa-pen', (req, res) => {
+  Record.find({ category: 'fa-pen' }, (err, records) => {
+    return res.render('fa-pen', { records: records })
+  })
+})
 
 app.post('/records', (req, res) => {
   let icon = ''
@@ -58,6 +83,37 @@ app.post('/records', (req, res) => {
   record.save(err => {
     if (err) return console.error(err)
     return res.redirect('/')
+  })
+})
+
+app.get('/records/:id/edit', (req, res) => {
+
+  let select = ({
+    home: '',
+    van: '',
+    grin: '',
+    utensils: '',
+    pen: ''
+  })
+
+  Record.findById(req.params.id, (err, record) => {
+    if (record.category === 'fa-home') {
+      select.home += 'select'
+    }
+    if (record.category === 'fa-shuttle-van') {
+      select.van += 'select'
+    }
+    if (record.category === 'fa-grin-beam') {
+      select.grin += 'select'
+    }
+    if (record.category === 'fa-utensils') {
+      select.utensils += 'select'
+    }
+    if (record.category === 'fa-pen') {
+      select.pen += 'select'
+    }
+    if (err) return console.error(err)
+    return res.render('edit', { record: record, select: select })
   })
 })
 
